034cfc70cca58a6be94a19342284bbbf75b77a08
oobruno <brunorossetti80@gmail.com>
2026-03-13 15:53:24 -0300

    atualizacao_aula3

--- aula13_03/aula13_03/aula13_03/main.c
+++ aula13_03/aula13_03/aula13_03/main.c
@@ -1,27 +1,28 @@
 /*
  * main.c
  *
- * Created: 3/13/2026 2:40:53 PM
+ * Created: 3/13/2026 2:22:59 PM
  *  Author: Aluno
  */ 
+
 #define F_CPU 16000000
+
 #include <xc.h>
 #include "avr/interrupt.h"
 #include "util/delay.h"
-ISR(INT0_vect){
-	PORTC ^= (1<<PORTC0);
+
+ISR(INT0_vect) {
+	PORTC = (1<<PORTC0); //Alterna LED
 	_delay_ms(100);
 	PORTC = 0;
 }
 
-int main(void)
-{
-	DDRC = (1<<DDC0);
-	EICRA = (1<<ISC01)|(1<<ISC00);
-	EIMSK = (1<<INT1)|(1<<INT0);
+int main(void) {
+	DDRC = (1<<DDC0);				// configura pino PC0 como sa?da (LED)
+	EICRA = (0<<ISC01)|(1<<ISC00);	// INT0: Ambas as bordas
+	EIMSK = (1<<INT1)|(1<<INT0);	// INT0 habilitada, INT1 desabilitada
 	sei();
-    while(1)
-    {
+    while(1) {
         //TODO:: Please write your application code 
     }
 }
