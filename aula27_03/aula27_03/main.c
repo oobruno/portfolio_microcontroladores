f6df8a7c6aabaf591421084e8909947999e600d8
oobruno <brunorossetti80@gmail.com>
2026-03-27 15:52:58 -0300

    codigo_completo_para_envio_de_mensagem

--- aula27_03/aula27_03/main.c
+++ aula27_03/aula27_03/main.c
@@ -9,24 +9,35 @@
 #include <xc.h>
 #include "util/delay.h"
 
-uint8_t gMessage[4] = {200,100,50,94}; //ultimo byte é checksum
+uint8_t gMessage[3] = {200,100,50};
+	
+void UART_sendMessage(uint8_t * pData, int pSize){
+	uint8_t * tMsgPtr = pData;
+	uint8_t tChecksum = 0;
+	int i;
+	for(i=0;i<pSize;i++){
+		while((UCSR0A & (1 <<UDRE0)) == 0);
+		uint8_t tMessageByte = *tMsgPtr;
+		tChecksum += tMessageByte;
+		UDR0 = tMessageByte;
+		tMsgPtr++;
+	}
+	while((UCSR0A & (1<<UDRE0)) ==0);
+	UDR0 = tChecksum;
+}
+// código completo para envio de mensagem!.
 
 int main(void){
 	UBRR0 = 103; //TAXA DE baud rate 9600
 	UCSR0A = (0<<U2X0); //
-	UCSR0B = (1<<TXEN0)|(0<<UCSZ02); //
+	UCSR0B = (1<<TXEN0)|(0<<UCSZ02); // habilita transmissor
 	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00) // MODO ASSINCRONO
 			| (1<<UPM01)|(0<<UPM00) //HAB PARIDADE PAR
 			| (0<<USBS0) // 1 BIT DE STOP
 			| (1<<UCSZ01)|(1<<UCSZ00); //FRAMES DE 8 BITS
 			
-    while(1)
-	{
-		int i;
-		for(i=0;i<4;i++){	
-		    UDR0 = gMessage[i];
-			_delay_ms(2);
-		}
-			_delay_ms (10);
+    while(1){
+		UART_sendMessage(gMessage, 3);
+	_delay_ms(10);
     }
 }
