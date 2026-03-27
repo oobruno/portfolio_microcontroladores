1953353ad0716e45065ae36a33047dd0c9b7d9f4
oobruno <brunorossetti80@gmail.com>
2026-03-27 17:19:23 -0300

    ultima_att

--- aula27_03/aulaparte2_27_03/main.c
+++ aula27_03/aulaparte2_27_03/main.c
@@ -9,9 +9,34 @@
 #include <xc.h>
 #include "util/delay.h"
 
+uint8_t gReceivedBuffer [4];
+void UART_receivedMessage(uint8_t * pBuffer, int pSize){
+	uint8_t tReceivedByte = UDR0;
+	uint8_t * tBufferPtr = pBuffer;
+	for(int i=0;i<pSize;i++){
+		while((UCSR0A & (1<<RXC0)) == 0)//ESPERA MSG CHEGAR
 
+		*tBufferPtr = tReceivedByte;
+		tBufferPtr++;
+	}
+}
+void UART_parsedMessage(uint8_t *pMessage){
+	//calcula checksum
+	uint8_t tChecksum = 0;
+	for(int i=0;i<3;i++){
+		tChecksum += pMessage[i];
+	}
+	if(pMessage[3] == tChecksum){
+		//checksum correto
+		PORTB |= (1<<PORTB0);
+	} else {
+		//erro de checksum
+		PORTB = (1<<PORTB2);
+	}
+
+}
 int main(void){
-	DDRB = (1<<DDB0); //PINO B0 SAIDA
+	DDRB = (1<<DDB0)|(1<<DDB2); //PINO B0 SAIDA
 	UBRR0 = 103; //TAXA DE baud rate 9600
 	UCSR0A = (0<<U2X0); //
 	UCSR0B = (1<<RXEN0)|(0<<UCSZ02); // habilita receptor
@@ -21,12 +46,9 @@ int main(void){
 	| (1<<UCSZ01)|(1<<UCSZ00); //FRAMES DE 8 BITS
 		
     while(1){
-		while((UCSR0A & (1<<RXC0)) == 0); //ESPERAR 1 BYTE(MENSAGEM) CHEGAR	
-		uint8_t tReceivedByte = UDR0;
-		if(tReceivedByte == 200){
-			PORTB |= (1<<PORTB0);	
-    } else {
 		PORTB = 0;
+		UART_parsedMessage(gReceivedBuffer);
+		UART_receivedMessage(gReceivedBuffer, 4);
+		_delay_ms(10);
 	}
 }
-	}
